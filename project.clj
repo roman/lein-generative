9c68483aa91709c7812899f2a07f00d314d5b3a0
Roman Gonzalez <romanandreg@gmail.com>
2011-12-28 15:31:12 -0800

    First basic version of lein generative

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject lein-generative "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dev-dependencies [[org.clojure/test.generative "0.1.3"]]
+  :eval-in-leiningen true)
+
---
+++ src/leiningen/generative.clj
@@ -0,0 +1,44 @@
+(ns leiningen.generative
+  (:refer-clojure :exclude [test])
+  (:require leiningen.core)
+  (:use [leiningen.compile :only [eval-in-project]]
+        [leiningen.classpath :only [classpath]]))
+
+(defn- run-generative-tests [project]
+  `(do
+    (println "Testing on" gen/*cores* "cores for" gen/*msec* "msec.")
+    (let [futures# (gen/test-dirs ~(:generative-path project))]
+      (doseq [f# futures#]
+        (try
+          @f#
+          (catch Throwable t#)
+            ;(.printStackTrace t#)
+            ;(System/exit -1))
+
+          (finally
+            (when-not ~leiningen.core/*interactive?*
+              (shutdown-agents))))))))
+
+(defn- set-generative-path-to-project [project]
+  (let [generative-path (str (:root project)
+                             java.io.File/separatorChar
+                             "generative")]
+    (merge {:generative-path generative-path} project)))
+
+(defn- add-generative-path-to-classpath [project]
+  (update-in project
+             [:extra-classpath-dirs]
+             #(conj % (:generative-path project))))
+
+(defn generative
+  "Run test.generative tests"
+  [project & _]
+  (let [new-project (-> project
+                        set-generative-path-to-project
+                        add-generative-path-to-classpath)]
+    (eval-in-project
+      new-project
+      (run-generative-tests new-project)
+      nil
+      nil
+      '(require '[clojure.test.generative :as gen]))))
