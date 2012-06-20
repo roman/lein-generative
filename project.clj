6e8962905c7060a9a09598e7d1798c841676c2c4
Nelson Morris <nmorris@nelsonmorris.net>
2012-06-20 14:00:53 -0500

    Updates for use in :plugins
    lein 1.7.0 and lein 2.0.0-previews use :plugins for leiningen plugins.
    Previously :dev-dependencies represented both plugins and dev-deps.
    
    Adding dependencies is now done with changing the project before
    eval-in-project.  See the leiningen plugins doc and lein-swank for
    more details.

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject lein-generative "0.1.4.1-SNAPSHOT"
   :description "test.generative runner for Clojure contrib's test.generative."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/test.generative "0.1.4"]]
   :eval-in-leiningen true)
 
