4cad7447eae08d13e4b4a9a1160822eb503f4ee5
Nelson Morris <nmorris@nelsonmorris.net>
2012-06-20 21:50:19 -0500

    Support lein1 and lein2
    Use resolve to load all namespace dependencies.  This is the standard
    way to check and load whats needed from the either lein1 or lein2.
    
    This ended up changing most of the function signatures.

--- src/leiningen/generative.clj
+++ src/leiningen/generative.clj
@@ -1,30 +1,25 @@
-(ns leiningen.generative
-  (:refer-clojure :exclude [test])
-  (:require leiningen.core)
-  (:use [leiningen.compile :only [eval-in-project]]
-        [leiningen.classpath :only [classpath]]))
+(ns leiningen.generative)
 
-(defn- run-generative-tests [project]
+(defn- run-generative-tests [paths keep-alive?]
   `(do
-     (let [path# ~(or (:generative-path project) (:test-path project))]
-       (println "Testing generative tests in" path#
-                "on" gen/*cores* "cores for"
-                gen/*msec* "msec.")
-       (let [futures# (gen/test-dirs path#)]
-         (doseq [f# futures#]
-           (try
-             @f#
-             (catch Throwable t#
-               (.printStackTrace t#)
-               (System/exit -1))
-             (finally
-              (when-not ~leiningen.core/*interactive?*
-                (shutdown-agents)))))))))
+     (println "Testing generative tests in" [~@paths]
+              "on" gen/*cores* "cores for"
+              gen/*msec* "msec.")
+     (let [futures# (gen/test-dirs ~@paths)]
+       (doseq [f# futures#]
+         (try
+           @f#
+           (catch Throwable t#
+             (.printStackTrace t#)
+             (System/exit -1))
+           (finally
+            (when-not ~keep-alive?
+              (shutdown-agents))))))))
 
-(defn- add-generative-path-to-classpath [project]
-  (update-in project
-             [:extra-classpath-dirs]
-             #(conj % (:generative-path project))))
+(defn- add-generative-path-to-classpath [project classpath-id]
+  (if-let [path (:generative-path project)]
+    (update-in project classpath-id #(conj % path))
+    project))
 
 (defn add-generative-dependency [project]
   (if (some #(= 'org.clojure/test.generative (first %)) (:dependencies project))
@@ -35,12 +30,26 @@
 (defn generative
   "Run test.generative tests"
   [project & _]
-  (let [new-project (-> project
-                        add-generative-dependency
-                        add-generative-path-to-classpath)]
-    (eval-in-project
-      new-project
-      (run-generative-tests new-project)
-      nil
-      nil
-      '(require '[clojure.test.generative :as gen]))))
+  (let [[eip keep-alive? classpath-id paths]
+        (or (try (require 'leiningen.core.eval)
+                 [(resolve 'leiningen.core.eval/eval-in-project)
+                  (not @(resolve 'leiningen.core.main/*exit-process?*))
+                  [:test-paths]
+                  (if-let [path (:generative-path project)]
+                    [path]
+                    (:test-paths project))]
+                 (catch java.io.FileNotFoundException _))
+            (try (require 'leiningen.compile)
+                 [(fn [p f g]
+                    ((resolve 'leiningen.compile/eval-in-project) p f nil nil g))
+                  @(resolve 'leiningen.core/*interactive?*)
+                  [:extra-classpath-dirs]
+                  [(or (:generative-path project)
+                       (:test-path project))]]
+                 (catch java.io.FileNotFoundException _)))]
+    (let [new-project (-> project
+                          add-generative-dependency
+                          (add-generative-path-to-classpath classpath-id))]
+      (eip new-project
+           (run-generative-tests paths keep-alive?)
+           '(require '[clojure.test.generative :as gen])))))
